fcab50826dd5c60d69ff6842b6a92bd450011415
Julia Egorova <julia.egorova@fincodeonline.com>
2012-11-23 16:18:24 +0400

    Added AmazonParser for parsing pages from amazon.com

---
+++ lib/amazon_parser.rb
@@ -0,0 +1,62 @@
+require 'open-uri'
+
+class AmazonParser
+
+  attr_accessor :url
+  attr_reader :doc
+
+  def initialize(url)
+    @url = url
+    @doc = Nokogiri::HTML(open(@url))
+  end
+
+  def parse
+    items = @doc.xpath('//*[@class="zg_item_normal"]')
+
+    items.each do |item| 
+      parse_item(item)
+    end
+  end
+
+  protected
+
+  def parse_item(item)
+    proposal = Proposal.new
+
+    proposal.title      = self.parse_title(item)
+    proposal.brand      = self.parse_brand(item)
+    proposal.rating     = self.parse_rating(item)
+    proposal.price      = self.parse_price(item)
+    proposal.image_url  = self.parse_image_url(item)
+
+    proposal.save!
+    proposal
+  end
+
+  # Parses for attributes
+
+  def parse_title(item)
+    title_item = item.xpath('.//*[@class="zg_title"]').first
+    title_item.content if title_item.present?
+  end
+
+  def parse_brand(item)
+    brand_item = item.xpath('.//*[@class="zg_byline"]').first
+    brand_item.content.delete('by ') if brand_item.present?
+  end
+
+  def parse_rating(item)
+    rating_item = item.xpath('.//*[@class="asinReviewsSummary acr-popover"]/*[1]//*[contains(@class, "swSprite")]/@title').first
+    rating_item.content.to_f if rating_item.present?
+  end
+
+  def parse_price(item)
+    price_item = item.xpath('.//*[@class="priceBlock"]/strong[text()="Price: "]/../*[@class="price"]').first
+    price_item.content.delete('$') if price_item.present?
+  end
+
+  def parse_image_url(item)
+    image_url_item = item.xpath('.//*[@class="zg_itemImage_normal"]//img/@src').first
+    image_url_item.content if image_url_item.present?
+  end
+end

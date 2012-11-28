377f0cf6f6f60bfb3d638c2ebc650cb16488dca0
Julia Egorova <julia.egorova@fincodeonline.com>
2012-11-28 18:23:22 +0400

    Added layout for wiselinks.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :current_layout
-
 protected
 
-  def current_layout
-    layout = self.send(:_layout)
-    # if layout.instance_of? String
-    #   layout
-    # else
-      layout.identifier
-    # end
-  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   def catalog
     @filter = Proposals::Search.new(params)
     @proposals = @filter.results.page(params[:page])
+
+    render :partial => 'proposals/table', :locals => { :proposals => @proposals } if request.headers['X-Slide'] == 'partial'
   end
 
   def about

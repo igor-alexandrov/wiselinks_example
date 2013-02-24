9df52d98168dec5ddbf397efa8306d2151c99a50
Igor Alexandrov <igor.alexandrov@gmail.com>
2013-02-24 19:48:01 +0400

    Added wiselinks version number

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,10 @@ module ApplicationHelper
   def set_title(value)
     content_for(:title, value)
 
-    # wiselinks_title(value) 
+    wiselinks_title(value) 
+  end
+
+  def wiselinks_version    
+    Gem.loaded_specs['wiselinks'].version.to_s rescue nil
   end
 end
